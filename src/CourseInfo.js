9d76656020b01a4712e0efddba1649c56af1304b
Joyce <jhuang3@uw.edu>
2016-12-03 17:43:35 -0800

    changes to course name display

--- src/CourseInfo.js
+++ src/CourseInfo.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
+//Uppercase letters credit to:
+//http://stackoverflow.com/questions/4878756/javascript-how-to-capitalize-first-letter-of-each-word-like-a-2-word-city
+function toCamelCase(str) {
+    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
 var CourseInfo = React.createClass({
-	render(){
+
+
+render(){
+	
 		return(
 			<div className="CourseInfo">
-				<p>Course Number: {this.props.number}</p> 
-				<p>Course Title: {this.props.name}</p> 
+				<h4>{this.props.number} {toCamelCase(this.props.name)}</h4> 
 				<p>General Requirement: {this.props.type}</p>
 				<p>Credits: {this.props.credits}</p> 
 			</div>
