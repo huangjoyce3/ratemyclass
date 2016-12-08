153c1ee5cadd36f4c0c5dfe4a5667a9a11f75a11
Christy Lu <hyl9@uw.edu>
2016-12-08 09:12:36 -0800

    search function

--- src/ReChart.js
+++ src/ReChart.js
@@ -4,17 +4,33 @@ import {ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, Legend} fro
 
 
 var ReChart = React.createClass({
+	getInitialState(){
+		return {search:''}
+	},
+
+	filterData(event){
+		var value = event.target.value;
+		this.setState({search:value})
+
+	},
 
 	render() {
+		var data = this.props.chartData,
+			search = this.state.search.trim().toLowerCase();
+		if(search.length > 0){
+			data = data.filter(function(data){
+				return data.professor.toLowerCase().match(search);
+			});
+		}
 
-		console.log(this.props.chartData);
 		return(
 			<div className="chart">
+				<input className="search" placeholder="Type in an instructor..." onChange={this.filterData} />
 				<ScatterChart width={500} height={500}
 	            	margin={{top: 5, right: 30, left: 20, bottom: 5}}>
 	            <XAxis dataKey={'difficulty'} name='difficulty'/>
 	            <YAxis dataKey={'workload'} name='workload'/>
-	            <Scatter name='review' data={this.props.chartData} fill='red' opacity='0.3' />
+	            <Scatter name='review' data={data} fill='red' opacity='0.3' />
 	            <CartesianGrid strokeDasharray="3 3" />
 	            <Tooltip cursor={{strokeDasharray: '3 3'}} />
 	            <Legend />
