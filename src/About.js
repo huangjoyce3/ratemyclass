cb7727b0a6d40c229b676b87ef27c19d945a01f8
Joyce <jhuang3@uw.edu>
2016-12-09 02:06:18 -0800

    modal dialog

--- src/About.js
+++ src/About.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import './css/About.css';
+import ReactDom from 'react-dom';
+import Modal from 'react-modal';
 
 var About = React.createClass({
+	getInitialState: function() {
+    return { modalIsOpen: false };
+	  },
+	 
+	  openModal: function() {
+	    this.setState({modalIsOpen: true});
+	  },
+	 
+	  afterOpenModal: function() {
+	    // references are now sync'd and can be accessed. 
+	    this.refs.subtitle.style.color = '#f00';
+	  },
+	 
+	  closeModal: function() {
+	    this.setState({modalIsOpen: false});
+	  },
 	render(){
 		return(
-			<div className="about">            
+			<div className="about">
+			    <button onClick={this.openModal}>Open Modal</button>
+			    <Modal
+		          isOpen={this.state.modalIsOpen}
+		          onAfterOpen={this.afterOpenModal}
+		          onRequestClose={this.closeModal}
+		          style={customStyles}
+		          contentLabel="Example Modal"
+		        >
 				<h3>About Us</h3>
+				<button onClick={this.closeModal}>close</button>
 				<div className="description">
 				<p>The purpose of this INFO 343 project is to ease the hassle of making college schedules. It helps students to find classes that best fit their academic goals and interests through connection with their peers. It is a one stop for all students needs for selecting classes. </p>
 				<p>Currently, this project focuses only on classes that are offered within iSchool.</p>
@@ -35,6 +62,7 @@ var About = React.createClass({
 						<p>Jing Wen Pan</p>
 					</div>
 				</div>
+				</Modal>
 			</div>
 		);
 	}
